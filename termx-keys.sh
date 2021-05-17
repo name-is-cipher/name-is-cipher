28646f84f3fc2a0da9ca4c9c80ab1be8248cb44c
Aravind Swami Potluri <aravindswami135@gmail.com>
2021-05-18 01:46:15 +0530

    my termux

--- termx-keys.sh
+++ termx-keys.sh
@@ -8,11 +8,27 @@ function print_banner() {
     printf "${blue}########## name-is-cipher ###########${reset}\n\n"
 }
 
+function progress {
+BAR='##############################'
+FILL='------------------------------'
+totalstrokes=100  # num. lines in file
+barLen=30
+# --- iterate over lines in csv file ---
+count=0
+while [[ $count -lt $totalstrokes ]]; do
+    # update progress bar
+    sleep 0.08
+    count=$(($count + 1))
+    percent=$((($count * 100 / $totalstrokes * 100) / 100))
+    i=$(($percent * $barLen / 100))
+    echo -ne "\r[${BAR:0:$i}${FILL:$i:barLen}] $count/$totalstrokes ($percent%)"
+done
+echo " "
+}
+
 print_banner
 
-
-
-# Termux Superuser
+######### Termux Superuser ##########
 
 echo "Installing Termux superuser ..."
 echo " "
@@ -21,26 +37,6 @@ echo " " >> $HOME/.bashrc
 echo "# This PATH is for Termux superuser bin folder" >> $HOME/.bashrc
 echo "export PATH=$PATH:/data/data/com.termux/files/home/.termux/bin" >> $HOME/.bashrc
 mkdir $HOME/.termux/bin
-function progress {
-BAR='##############################'
-FILL='------------------------------'
-totalstrokes=100  # num. lines in file
-barLen=30
-
-# --- iterate over lines in csv file ---
-count=0
-while [[ $count -lt $totalstrokes ]]; do
-
-    # update progress bar
-    sleep 0.08
-    count=$(($count + 1))
-    percent=$((($count * 100 / $totalstrokes * 100) / 100))
-    i=$(($percent * $barLen / 100))
-    echo -ne "\r[${BAR:0:$i}${FILL:$i:barLen}] $count/$totalstrokes ($percent%)"
-
-done
-echo " "
-}
 # Making xsu
 echo "# This file starts termux in su with all termux binaries enabled" >> $HOME/.termux/bin/xsu
 echo " " >> $HOME/.termux/bin/xsu
@@ -56,12 +52,31 @@ echo "Installation successful !!!"
 echo " "
 echo "> Run 'xsu' anywhere to start Termux Superuser."
 echo " "
+################################
 
-# Boot nethunter
+
+echo " --------------------------"
+echo " "
+
+###### Termux Extra keys #######
+
+echo "Adding Extra Keys to Termux !!!"
+echo " " >> .termux/termux.properties
+echo "extra-keys = [ \ " >> .termux/termux.properties
+echo " ['CTRL','$','|','HOME','UP','END','-','='], \ " >> .termux/termux.properties
+echo " ['TAB','ESC','ALT','LEFT','DOWN','RIGHT','/','DEL'] \ " >> .termux/termux.properties
+echo "]" >> .termux/termux.properties
+progress
+#################################
 
 
+
+
+###### END of installation #######
+
+echo " "
 echo " [*] Termux needs to be restarted to work properly,"
 echo "     Please restart !"
 echo " "
-
+#################################
 
